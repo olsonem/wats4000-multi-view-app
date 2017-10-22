b6814c06a625f48f548beafa1d127bf40dbe572c
olsonem <olsonem@seattleu.edu>
2017-10-21 20:29:55 -0700

    completed project

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Survey from '@/components/Survey'
+import Secret from '@/components/Secret'
 // TODO: Import the Survey component
 // TODO: Import the Secret component
 
@@ -12,8 +14,17 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/survey',
+      name: 'Survey',
+      component: Survey
+    },
+    {
+      path: '/secret',
+      name: 'Secret',
+      component: Secret
     }
-    // Add the Survey route definition
-    // Add the Secret route definition
+   
   ]
 })
